e51c9cd45c1036c05a8b26ef9d6df9eadcd489a5
AliPea <pearseindeas@gmail.com>
2017-09-10 20:36:15 -0700

    first component, directive and service

---
+++ quick-app/src/app/services/movieslist/movieslist.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MovieslistService {
+  listItems: any[] = ['Movie One', 'Movie Two', 'Movie Three', 'Movie Four'];
+  constructor() { }
+    getMovies() {
+      return this.listItems;
+    }
+  }
